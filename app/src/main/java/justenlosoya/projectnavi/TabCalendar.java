ae0e7ca37440dc84de30efee6fdb9681ec30af85
Eden <TGPforeva@Gmail.com>
2017-10-20 10:59:05 -0700

    Pre-Alpha 1.0
    Reconstruction of the beginning of the entire app. Original was damaged and thus defective.

---
+++ app/src/main/java/justenlosoya/projectnavi/TabCalendar.java
@@ -0,0 +1,20 @@
+package justenlosoya.projectnavi;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by justenlosoya on 10/20/17.
+ */
+
+public class TabCalendar extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_calendar, container, false);
+    }
+}
