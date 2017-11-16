2f3171c78d597f9ebc008efcb4f69ed9517ec7e2
Eden <TGPforeva@Gmail.com>
2017-11-16 11:14:34 -0800

    Scrolling&Zooming
    Added scrolling and zooming to the GPS image. However, it seems laggy in the emulator, so that will need to be investigated later. I'm also working on implementing the other map and an option to switch between the two, then I will begin working on the GPS system.

--- app/src/main/java/justenlosoya/projectnavi/TabMap.java
+++ app/src/main/java/justenlosoya/projectnavi/TabMap.java
@@ -17,4 +17,5 @@ public class TabMap extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.tab_map, container, false);
     }
+
 }
