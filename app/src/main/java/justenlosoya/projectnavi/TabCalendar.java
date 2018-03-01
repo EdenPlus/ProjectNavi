2260fc62f6a41ca4f5ce4b1f62bdfced2078cfc4
Eden <TGPforeva@Gmail.com>
2018-03-01 10:35:53 -0800

    Example of changes to fragments in runtime

--- app/src/main/java/justenlosoya/projectnavi/TabCalendar.java
+++ app/src/main/java/justenlosoya/projectnavi/TabCalendar.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.Random;
+
 /**
  * Created by justenlosoya on 10/20/17.
  */
@@ -19,7 +21,14 @@ public class TabCalendar extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         inf = inflater.inflate(R.layout.tab_calendar, container, false);
         // Start Layout Preference Changes
-
+        try {
+            TextView t = (TextView) inf.findViewById(R.id.textViewC);
+            Random r = new Random();
+            int i1 = r.nextInt(100);
+            t.setText("Randomly generated number when the fragment is loaded: " + i1);
+        } catch (NullPointerException e) {
+            // Ugh
+        }
         // End layout Preference Changes
         return inf;
     }
